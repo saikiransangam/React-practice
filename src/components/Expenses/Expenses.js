1c616f57ebd4840e85be39c9c0520abeeab5ade2
Sangam <sai.kiran.sangam@autozone.com>
2023-04-06 13:41:33 -0500

    Expenses Component"

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import './Expenses.css';
 
 function Expenses(props) {
     
     return (
-        <div>
-          <h1>Express Item!</h1>
+        <Card className="expenses">
           <ExpenseItem 
           title = {props.items[0].title}
           amount = {props.items[0].amount}
@@ -25,7 +27,7 @@ function Expenses(props) {
           amount = {props.items[3].amount}
           date = {props.items[3].date}
           ></ExpenseItem>
-        </div>
+        </Card>
     )
 }
 export default Expenses;
